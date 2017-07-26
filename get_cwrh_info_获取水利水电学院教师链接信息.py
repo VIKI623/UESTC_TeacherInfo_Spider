2cfb0122eec5524bde3f67f15c4b169042445d4d
VIKI ZHAO <1131474161@qq.com>
2017-07-26 15:15:42 +0530

    V1

---
+++ get_cwrh_info_获取水利水电学院教师链接信息.py
@@ -0,0 +1,28 @@
+#coding=utf-8
+import requests
+import re
+from bs4 import BeautifulSoup
+import json
+
+url_base = "http://cwrh.scu.edu.cn"
+
+def getHtml(url):
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+        r.encoding = r.apparent_encoding
+        return r.text
+    except:
+        return 'error'
+
+
+if __name__ == "__main__":
+     html = getHtml(url_base + "/home/teacher/index.html")
+     fp = open('D:/data/sichuanU/slsd.txt','w')
+     if(html!='error'):
+            soup = BeautifulSoup(html,"html.parser")
+            content = soup.find_all('div',class_="menu")
+            TeacherList = content[1].find_all('a',href=re.compile(u"/home/teacher/details/id/"))
+            for Teacher in TeacherList:
+                fp.write(url_base + Teacher["href"] + "\n")
+            fp.close
